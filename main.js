0eeaf57dbb6d5e06adcbcd05b0549d8fa8a81de5
maerli <maerli.pereira@gmail.com>
2018-04-17 13:39:48 -0300

    editando

--- main.js
+++ main.js
@@ -24,23 +24,14 @@ let training_data = [{
 ];
 
 let nn = new NeuralNetwork(2, 4, 1);
+nn.setLearningRate(0.01);
+
 
 for (let i = 0; i < 10; i++) {
     let index = Math.floor(random(0,training_data.length));
     let data = training_data[index];;
     nn.train(data.inputs, data.outputs);
   }
- nn.setLearningRate(0.01);
-
-  let resolution = 10;
-  let cols = 100 / resolution;
-  let rows = 100 / resolution;
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      let x1 = i / cols;
-      let x2 = j / rows;
-      let inputs = [x1, x2];
+      let inputs = [0, 0];
       let y = nn.predict(inputs);
       console.log(y);
-    }
-  }
